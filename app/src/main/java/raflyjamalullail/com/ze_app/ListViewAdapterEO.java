3a486caa6994fa7b9e5cc39a66996c1dfaceef0b
Muhammad Rafly J <50012708+mraflytj@users.noreply.github.com>
2019-06-22 13:01:47 +0700

    hip hip hore

--- app/src/main/java/raflyjamalullail/com/ze_app/ListViewAdapterEO.java
+++ app/src/main/java/raflyjamalullail/com/ze_app/ListViewAdapterEO.java
@@ -2,7 +2,6 @@ package raflyjamalullail.com.ze_app;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
